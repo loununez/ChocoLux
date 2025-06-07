88fbbe0a22d54dfa92369b3c66fed1e4a9d37d51
Lourdes Nuñez <114270220+loununez@users.noreply.github.com>
2025-06-07 18:37:35 -0300

    Add files via upload

---
+++ menu_responsivo.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById('hamburger');
+  const nav = document.getElementById('main-nav');
+
+  hamburger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
